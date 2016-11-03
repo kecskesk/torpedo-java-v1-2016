cbef057089856053cd8752055d678cc0e2c932fe
Dombi Soma Kristóf <dombis@sonrisa.hu>
2016-11-03 20:25:04 +0100

    Add timed functionality with Timer class
    Also added game-creation logic.

--- src/main/java/com/torpedogame/v1/App.java
+++ src/main/java/com/torpedogame/v1/App.java
@@ -3,16 +3,53 @@ package com.torpedogame.v1;
 import com.torpedogame.v1.service.GameAPI;
 import com.torpedogame.v1.service.GameApiImpl;
 
+import com.torpedogame.v1.model.protocol.CreateGameResponse;
+import com.torpedogame.v1.model.protocol.GameInfoResponse;
+import com.torpedogame.v1.model.protocol.JoinGameResponse;
+import com.torpedogame.v1.service.GameAPI;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends TimerTask
 {
+    private static GameAPI gameEngine;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "The torpedo program is starting..." );
+
+        // Create a game
+        CreateGameResponse currentGame = gameEngine.createGame();
+        System.out.println(currentGame.getMessage());
+
+        // Join to the created game
+        JoinGameResponse joinGameResponse = gameEngine.joinGame(currentGame.getId());
+        System.out.println(joinGameResponse.getMessage());
+
+        // Get game-info
+        GameInfoResponse gameInfoResponse = gameEngine.gameInfo(currentGame.getId());
+
+        // Start executing the steps for each round
+        // This will start the execution immediately, if a little delay is needed,
+        // increase the second parameter of the schedule function.
+        Timer timer = new Timer();
+        timer.schedule(new App(), 0, gameInfoResponse.getMapConfiguration().getRoundLength());
+
         GameAPI gameAPI = new GameApiImpl();
         System.out.print(gameAPI.createGame().toString());
+    }
+
+    /**
+     * This function will be called periodically.
+     */
+    @Override
+    public void run() {
+        // TODO implement
+        System.out.println("Starting round {insert round number here}");
     }
 }
